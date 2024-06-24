26923a175148127725301d4beaee86999a77c1a7
avinashsharma <avinashsharma.np@gmail.com>
2024-06-24 10:26:13 -0400

    update 1

--- number_guesser.py
+++ number_guesser.py
@@ -1,16 +1,38 @@
 import random
 
-# Request an upper range of number from a user
-random_number_request = input("Please provide a number for a range: ")
+# UPPER LIMIT NUMBER
+upper_number = input("Please provide upper limit number: ")
 
 # Make sure input is a digit and convert to an integer.
-if random_number_request.isdigit():
-    random_number_request = int(random_number_request)
+if upper_number.isdigit():
+    upper_number  = int(upper_number)
     # 0 Inputs are not allowed for a upper range
-    if random_number_request <= 0:
+    if upper_number  <= 0:
         print("Please enter a number greater then 0")
 
-    print(random_number_request)
+    # user upper number to generate random number
+    else:
+        random_number = random.randint(0, upper_number)
+        print(random_number)
 
 else:
     print("Not a valid number, please enter a numeric value.")
+
+
+# GUESS NUMBER
+guess_number = input("Please guess a number: ")
+
+# # Make sure input is a digit and convert to an integer.
+# if guess_number.isdigit():
+#     guess_number  = int(guess_number)
+#     # 0 Inputs are not allowed for a upper range
+#     if guess_number  <= 0:
+#         print("Please enter a number greater then 0")
+#
+#     else:
+#         quit()
+
+
+if upper_number == guess_number:
+        print ()
+
