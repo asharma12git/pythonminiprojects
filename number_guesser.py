4fcf245694b687421a0db606c566aec2b3f73312
b59884 <avinash.sharma@cox.com>
2024-06-24 15:34:03 -0400

    random number check added

--- number_guesser.py
+++ number_guesser.py
@@ -5,34 +5,33 @@ upper_number = input("Please provide upper limit number: ")
 
 # Make sure input is a digit and convert to an integer.
 if upper_number.isdigit():
-    upper_number  = int(upper_number)
+    upper_number = int(upper_number)
     # 0 Inputs are not allowed for a upper range
-    if upper_number  <= 0:
+    if upper_number <= 0:
         print("Please enter a number greater then 0")
 
     # user upper number to generate random number
     else:
         random_number = random.randint(0, upper_number)
-        print(random_number)
-
+        print(f'random_number is {random_number}')
 else:
     print("Not a valid number, please enter a numeric value.")
 
-
 # GUESS NUMBER
-guess_number = input("Please guess a number: ")
+while True:
 
-# # Make sure input is a digit and convert to an integer.
-# if guess_number.isdigit():
-#     guess_number  = int(guess_number)
-#     # 0 Inputs are not allowed for a upper range
-#     if guess_number  <= 0:
-#         print("Please enter a number greater then 0")
-#
-#     else:
-#         quit()
+    guess_number = input("Please guess a number: ")
 
+    # Make sure input is a digit and convert to an integer.
+    if guess_number.isdigit():
+        guess_number = int(guess_number)
+    else:
+        print(f"Please Enter a valid number")
 
-if upper_number == guess_number:
-        print ()
-
+    # Compare guess with random
+    if guess_number == random_number:
+        print("Congrats! You have won")
+        break
+    else:
+        print("please try again")
+        continue
